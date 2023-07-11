deb5521bf89b5937246975079df1ee410ac8abc5
Luiz-Guilherme2907 <luizguilhermemodler2907@gmail.com>
2023-07-10 21:58:33 -0300

    game seed

--- src/main/java/com/devsuperior/demo/entities/Game.java
+++ src/main/java/com/devsuperior/demo/entities/Game.java
@@ -18,24 +18,30 @@ public class Game {
 	private long id;
 	private String title;
 	
-	@Column(name = "game_yaer")
-	private Integer yaer;
+	@Column(name = "game_year")
+	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {}
 
-	public Game(long id, String title, Integer yaer, String genre, String platform, String imgUrl,
+	public Game(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
-		this.yaer = yaer;
+		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -58,11 +64,11 @@ public class Game {
 	}
 
 	public Integer getYaer() {
-		return yaer;
+		return year;
 	}
 
-	public void setYaer(Integer yaer) {
-		this.yaer = yaer;
+	public void setYear(Integer year) {
+		this.year = year;
 	}
 
 	public String getGenre() {
@@ -73,12 +79,23 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	
+	
+	
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
 	}
 
 	public String getImgUrl() {
